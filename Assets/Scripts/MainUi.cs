ecad21e40618c5525e488f2199cb517c920277cf
M <shubhamvaity69@gmail.com>
2025-07-28 12:40:21 +0530

    faw

--- Assets/Scripts/MainUi.cs
+++ Assets/Scripts/MainUi.cs
@@ -1,13 +1,44 @@
+using System.Collections;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class MainUi : MonoBehaviour
 {
-   
 
 
-    public void playGame()
+    public GameObject mainUi;
+    public GameObject CutUI;
+
+    public TextMeshProUGUI textComponent;
+    public float typeSpeed = 0.05f;
+    public string fullText;
+
+    public void PlayButton()
     {
+        mainUi.SetActive(false);
+        CutUI.SetActive(true);
+        StartCoroutine(TypeText());
+    }
+
+
+    IEnumerator TypeText()
+    {
+        textComponent.text = "";
+        foreach (char c in fullText)
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        StartCoroutine(playGame());
+    }
+
+    
+
+
+     IEnumerator playGame()
+    {
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadSceneAsync(0);
     }
 
