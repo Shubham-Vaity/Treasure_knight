9daf7047f03efdaaad3110cca071dc7730ba08cb
M <shubhamvaity69@gmail.com>
2025-07-30 01:41:40 +0530

    SAGDSF

--- Assets/Enemy/Map3/Boss/Boss3.cs
+++ Assets/Enemy/Map3/Boss/Boss3.cs
@@ -14,19 +14,26 @@ public class BossPatrol : MonoBehaviour
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
+    public GameObject bombPrefab;           // Bomb prefab to spawn
+    public float jumpCooldown = 3f;         // Time interval between random jumps
+
     private Rigidbody2D rb;
     private bool movingToEnd = true;
     private bool facingRight = true;
 
+    private float jumpTimer = 0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimer = Random.Range(1f, jumpCooldown); // Randomize first jump
     }
 
     void FixedUpdate()
     {
         Patrol();
         DetectAndJump();
+        HandleRandomJump();
     }
 
     void Patrol()
@@ -54,11 +61,33 @@ public class BossPatrol : MonoBehaviour
         Vector2 rayOrigin = transform.position;
 
         RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayLength, playerLayer);
-        Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.red); // Visible ray in Scene view
+
+        Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.red);
 
         if (hit.collider != null && hit.collider.CompareTag("Player") && IsGrounded())
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            JumpAndSpawnBomb();
+        }
+    }
+
+    void HandleRandomJump()
+    {
+        jumpTimer -= Time.fixedDeltaTime;
+
+        if (jumpTimer <= 0f && IsGrounded())
+        {
+            JumpAndSpawnBomb();
+            jumpTimer = Random.Range(2f, jumpCooldown + 2f); // Randomize next jump
+        }
+    }
+
+    void JumpAndSpawnBomb()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+        if (bombPrefab != null && groundCheck != null)
+        {
+            Instantiate(bombPrefab, groundCheck.position, Quaternion.identity);
         }
     }
 
---
+++ Assets/Enemy/Map4/AllenemySpr/prefabs/spawnLightning.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class spawnLightning : MonoBehaviour
+{
+    
+    public GameObject lightning;
+    public GameObject firespot;
+
+
+    void Start()
+    {
+        StartCoroutine(AttackLoop());
+
+    }
+
+
+    void rnd()
+    {
+        int randomNumber = Random.Range(0, 100);
+        if(randomNumber > 90)
+        {
+            Instantiate(lightning, firespot.transform.position, firespot.transform.rotation);
+        }
+        StartCoroutine(AttackLoop());
+    }
+
+    IEnumerator AttackLoop()
+    {
+        yield return new WaitForSeconds(2f);
+        rnd();
+
+    }
+}
